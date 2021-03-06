3c157bca5226bd5123ba4c89daa3aced57370acd
Akhil Goyal <akhilgoyal@macbook-pro.lan>
2021-03-06 18:59:43 -0500

    Displayed products page and product by ID from server side.

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import products from '../products';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import Product from '../components/Product';
 
 import { Row, Col } from 'react-bootstrap';
 
 const HomeScreen = () => {
+
+    const [products, setProducts] = useState([]);
+
+    useEffect(() => {
+
+        const fetchProducts = async() => {
+            const res = await axios.get('/api/products')
+            setProducts(res.data);
+        }
+
+        fetchProducts();
+
+    },[])
+
     return (
         <>
             <h1>Latest Products</h1>
